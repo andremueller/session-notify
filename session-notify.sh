1b318321834b844662bfe40661b8b714370baf02
Andre Mueller <andre@kiwisound.de>
2023-03-12 10:31:10 +0100

    Add basic notify script

---
+++ session-notify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# my bash template
+set -o errexit
+set -o nounset
+shopt -s nullglob
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+function die() {
+    echo "ERROR $? IN ${BASH_SOURCE[0]} AT LINE ${BASH_LINENO[0]}" 1>&2
+    exit 1
+}
+trap die ERR
+
+source /etc/pushover.d/session-notify.sh
+
+# requires the following environment variables
+# PUSHOVER_API_TOKEN
+# PUSHOVER_USER_KEY
+
+message="${1:-no args}"
+
+curl -s \
+  --form-string "token=${PUSHOVER_API_TOKEN}" \
+  --form-string "user=${PUSHOVER_USER_KEY}" \
+  --form-string "message=\"$message\"" \
+  https://api.pushover.net/1/messages.json
